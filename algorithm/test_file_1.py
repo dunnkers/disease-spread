c789cb953f08bf90b0e855bcc803e55218eb8b72
dunnkers <jeroenoverschie@gmail.com>
2020-02-27 13:28:53 +0100

    Add example of one data piece

--- algorithm/test_file_1.py
+++ algorithm/test_file_1.py
@@ -1,2 +1,11 @@
 x = 15
 print(x)
+
+datapiece = {
+    "longitude": 22112,
+    "latitude": 22131,
+    "size_width": 1000, # meters
+    "size_height": 1000, # meters
+    "population_count": 82821,
+    "poverty_index": 2911
+}
