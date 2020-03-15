3febcf3ef7b2fad4c8fa3bdd1e348fea0aa592a2
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-03-15 13:32:00 +0100

    According to spark notebook

--- spark-playground/main.py
+++ spark-playground/main.py
@@ -1,31 +1,16 @@
 import os
-
-# Make sure you call it before importing pyspark
 import findspark
-findspark.init() # no parameter means it should use SPARK_HOME
-# if doesn't work, try setting to /opt/spark-xxxxxxxx
+findspark.init()
 
 # Defining the python version to use on the workers
 os.environ['PYSPARK_PYTHON'] = '/usr/bin/python'
 
-# Pyspark imports
 import pyspark
-from pyspark.sql import SparkSession
-
-# For pi test
-import random
-
-spark = SparkSession.builder.master('spark://my-spark-master:7077').appName('spark-cluster').getOrCreate()
-
-NUM_SAMPLES = 10
-
-def inside(p):
-    x, y = random.random(), random.random()
-    return x*x + y*y < 1
-
-count = spark.sparkContext.parallelize(range(0, NUM_SAMPLES)) \
-             .filter(inside).count()
-
-print('Pi is roughly {}'.format(4.0 * count / NUM_SAMPLES))
-
-# Pi is roughly 4.0
+from pyspark import SparkContext
+sc = SparkContext("spark://my-spark-master:7077", "Pi Leibniz")
+iteration=10000
+partition=4
+data = range(0,iteration)
+distIn = sc.parallelize(data,partition)
+result=distIn.map(lambda n:(1 if n%2==0 else -1)/float(2*n+1)).reduce(lambda a,b: a+b)
+print("Pi is %f" % (result*4))
