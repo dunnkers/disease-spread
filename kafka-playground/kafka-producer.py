a48bdd92cbc6b2b73b134afa7e0317c7b3d0d89e
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-03-23 20:48:16 +0100

    Delete kafka playground folder

--- kafka-playground/kafka-producer.py
+++
@@ -1,4 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-producer.send('sample', b'Hello World')
-producer.send('sample', key=b'message-two', value=b'This is Kafka-Python')
