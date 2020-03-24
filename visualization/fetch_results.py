30eaa93be0f0730fd2a0651f024e71484e609075
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-03-24 13:29:38 +0100

    Debug printing

--- visualization/fetch_results.py
+++ visualization/fetch_results.py
@@ -2,10 +2,14 @@ from pymongo import MongoClient
 import json
 from os import getenv
 
+print('Instantiating mongodb connection...')
+
 password = getenv('MONGODB_ROOT_PASSWORD')
 client = MongoClient('mongodb://root:{}@my-mongodb:27017'.format(password))
 db = client['geotest-db']
 results_collection = db['results']
+
+print('Mongodb connection established! Waiting for changes to write to `results.json`.')
 
 with db['results'].watch() as stream:
     for change in stream:
