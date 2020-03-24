dc6e31ec9dc20973115fb05b89c8872782883dcf
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-03-24 13:18:20 +0100

    Add a visualization deployment ✌

---
+++ visualization/fetch_results.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+import json
+
+password = getenv('MONGODB_ROOT_PASSWORD')
+client = MongoClient('mongodb://root:{}@my-mongodb:27017'.format(password))
+db = client['geotest-db']
+results_collection = db['results']
+
+with db['results'].watch() as stream:
+    for change in stream:
+        # Something changed in the `results` collection! :)
+        # -> so, dump collection to json
+        res = results_collection.find({}, {'_id': 0})
+        collection = {
+            'type': 'FeatureCollection',
+            'features': list(res)
+        }
+        with open('./results.json', 'w') as outfile:
+            json.dump(collection, outfile)
